2c468e96f2a49441d888b327a68f0fc85c1ad578
MullaN <admullan@gmail.com>
2020-10-08 11:47:44 -0700

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let errorModal = document.getElementById('modal');
+errorModal.className = 'hidden';
+
+let hearts = document.getElementsByClassName('like-glyph');
+for (let i = 0; i < hearts.length; i++){
+  hearts[i].addEventListener('click', function(event){
+    changeOfHeart(event);
+  })
+}
+
+function changeOfHeart(event){
+  let currentHeart = event.target.textContent;
+  if (currentHeart == EMPTY_HEART){
+    mimicServerCall()
+      .then(function(){
+        event.target.textContent = FULL_HEART;
+        event.target.className = 'activated-heart'
+      })
+      .catch(function(error){
+        errorModal.innerText = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => errorModal.className = 'hidden', 5000);
+      });
+  } else {
+    event.target.textContent = EMPTY_HEART;
+    event.target.className = 'like-glyph'
+  }
+}
 
 
 
